8a195e3b6b471cb6ab7a16b65f2b6b520adefa47
Glitch (abdelrahman-dev) <none>
2020-02-04 01:01:52 +0000

    Restyle menu

--- main/Dish.jsx
+++ main/Dish.jsx
@@ -7,23 +7,38 @@ class Dish extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: false
+      clicking: false,
+      hovering: false
     };
   }
-
-  isSelected() {
-    return this.state.selected;
-  }
-
-  toggle() {
-    this.setState({ selected: !this.isSelected() });
+  
+  // I hate to put the style logic here instead keeping it 100% in css,
+  // but this is the least confusing way I found to
+  // fix the .dish:active issue while keeping the hover style mutually exclusive with .dishSelected and .dishUnselected
+  getStyle() {
+    // The clicking variable fixes an issue where .dish:active wouldn't turn off when you drag an image
+    if (this.state.clicking) {
+      return "dish dishSelected"
+    }
+    // Use this instead of .dish:hover to make the code more explicit by avoiding using weird specificity rules
+    if (this.state.hovering) {
+      return "dish dishHalfSelected"
+    }
+    if (this.props.selected) {
+      return "dish dishSelected"
+    }
+    return "dish dishUnselected"
   }
 
   render() {
     return (
       <div
-        className={"dish" + (this.isSelected() ? " selectedDish" : "")}
-        onClick={() => this.toggle()}
+        className={this.getStyle()}
+        onMouseDown={() => this.setState({clicking: true})}
+        onMouseUp={() => this.setState({clicking: false})}
+        onMouseEnter={() => {this.setState({hovering: true})}}
+        onMouseLeave={() => this.setState({hovering: false, clicking: false})}
+        onClick={this.props.onClick}
       >
         <div>
           <img src={this.props.image} />
--- main/entry.jsx
+++ main/entry.jsx
@@ -3,7 +3,6 @@
 const ReactDOM = require("react-dom");
 // Idk why webpack makes me require React here, but I get "Error: React is not defined" otherwise
 const React = require("react");
-const Form = require("./Form.jsx");
 const QuantityInput = require("./QuantityInput.jsx");
 const Dish = require("./Dish.jsx")
 
@@ -15,12 +14,14 @@ ReactDOM.render(
           "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Sweetsourchickensoaked.jpg/220px-Sweetsourchickensoaked.jpg"
         }
         name="Sweet 'n Sour Chicken"
+        selected={true}
       />
       <Dish
         image={
           "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Chicken_makhani.jpg/220px-Chicken_makhani.jpg"
         }
         name="Butter Chicken"
+        selected={false}
       />
     </div>
     <div>
