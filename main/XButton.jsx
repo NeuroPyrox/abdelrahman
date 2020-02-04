b870574d2bce75fe9b9514d7bee1aeb99b4344dc
Glitch (abdelrahman-dev) <none>
2020-02-04 03:46:35 +0000

    Make x button

---
+++ main/XButton.jsx
@@ -0,0 +1,12 @@
+"use strict";
+
+const React = require("react");
+
+const XButton = props => (
+  <button className="xButton">
+    <div />
+    <div />
+  </button>
+);
+
+module.exports = XButton;
--- main/entry.jsx
+++ main/entry.jsx
@@ -4,7 +4,8 @@ const ReactDOM = require("react-dom");
 // Idk why webpack makes me require React here, but I get "Error: React is not defined" otherwise
 const React = require("react");
 const QuantityInput = require("./QuantityInput.jsx");
-const Dish = require("./Dish.jsx")
+const Dish = require("./Dish.jsx");
+const XButton = require("./XButton.jsx");
 
 ReactDOM.render(
   <div>
@@ -25,23 +26,24 @@ ReactDOM.render(
       />
     </div>
     <div>
-      <h4>Sweet 'n Sour Chicken</h4>
+      <XButton />
+      <h4>10x Sweet 'n Sour Chicken</h4>
       <div className="spiceRadio">
         <input type="radio" name="snsSpice" value="notSpicy" id="snsNotSpicy" />
-        <label for="snsNotSpicy">Not Spicy</label>
+        <label htmlFor="snsNotSpicy">Not Spicy</label>
         <input type="radio" name="snsSpice" value="mild" id="snsMild" />
-        <label for="snsMild">Mild</label>
+        <label htmlFor="snsMild">Mild</label>
         <input type="radio" name="snsSpice" value="hot" id="snsHot" />
-        <label for="snsHot">Hot</label>
+        <label htmlFor="snsHot">Hot</label>
       </div>
-      <h4>Butter Chicken</h4>
+      <h4>4x Butter Chicken</h4>
       <div className="spiceRadio">
         <input type="radio" name="bcSpice" value="notSpicy" id="bcNotSpicy" />
-        <label for="bcNotSpicy">Not Spicy</label>
+        <label htmlFor="bcNotSpicy">Not Spicy</label>
         <input type="radio" name="bcSpice" value="mild" id="bcMild" />
-        <label for="bcMild">Mild</label>
+        <label htmlFor="bcMild">Mild</label>
         <input type="radio" name="bcSpice" value="hot" id="bcHot" />
-        <label for="bcHot">Hot</label>
+        <label htmlFor="bcHot">Hot</label>
       </div>
     </div>
   </div>,
