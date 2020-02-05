0f97bc36be3a361ae331e45fc70054e530aa5f62
Glitch (abdelrahman-dev) <none>
2020-02-05 08:24:56 +0000

    Make menu table rows editable

--- admin/menuTable.jsx
+++ admin/menuTable.jsx
@@ -4,27 +4,84 @@ const React = require("react");
 const XButton = require("../components/xButton.jsx");
 const PlusButton = require("../components/plusButton.jsx");
 
+// The immutable equivalent of object[key] = value
+const replaceField = (object, key, value) => {
+  const copy = Object.assign({}, object);
+  copy[key] = value;
+  return copy;
+};
+
+// The immutable equivalent of array[i] = value
+const replaceIndex = (array, i, value) => {
+  const copy = array.slice();
+  copy[i] = value;
+  return copy;
+};
+
+const Row = props => (
+  <tr>
+    <td>
+      <input
+        value={props.dish.name}
+        onChange={e =>
+          props.onChange(replaceField(props.dish, "name", e.target.value))
+        }
+      />
+    </td>
+    <td>
+      <input
+        type="checkbox"
+        checked={props.dish.spicy}
+        onChange={e =>
+          props.onChange(replaceField(props.dish, "spicy", e.target.checked))
+        }
+      />
+    </td>
+    <td>
+      <input
+        value={props.dish.imageUrl}
+        onChange={e =>
+          props.onChange(replaceField(props.dish, "imageUrl", e.target.value))
+        }
+      />
+    </td>
+    <td>
+      <XButton />
+    </td>
+  </tr>
+);
+
 class MenuTable extends React.Component {
   constructor(props) {
     super(props);
+    // TODO get this data from the server instead
     this.state = {
       rows: [
         {
           key: 1,
-          dish: "Sweet 'n Sour Chicken",
-          spicy: false,
-          imageUrl:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Sweetsourchickensoaked.jpg/220px-Sweetsourchickensoaked.jpg"
+          dish: {
+            name: "Sweet 'n Sour Chicken",
+            spicy: false,
+            imageUrl:
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Sweetsourchickensoaked.jpg/220px-Sweetsourchickensoaked.jpg"
+          }
         },
         {
           key: 2,
-          dish: "Butter Chicken",
-          spicy: true,
-          imageUrl:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Chicken_makhani.jpg/220px-Chicken_makhani.jpg"
+          dish: {
+            name: "Butter Chicken",
+            spicy: true,
+            imageUrl:
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Chicken_makhani.jpg/220px-Chicken_makhani.jpg"
+          }
         }
       ]
     };
+  }
+
+  replaceRow(i, newDish) {
+    const newRow = { key: this.state.rows[i].key, dish: newDish };
+    this.setState({ rows: replaceIndex(this.state.rows, i, newRow) });
   }
 
   render() {
@@ -38,26 +95,19 @@ class MenuTable extends React.Component {
           </tr>
         </thead>
         <tbody>
-          {this.state.rows.map(({ key, dish, spicy, imageUrl }) => (
-            <tr key={key}>
-              <td>
-                <input value={dish} />
-              </td>
-              <td>
-                <input type="checkbox" checked={spicy} />
-              </td>
-              <td>
-                <input value={imageUrl} />
-              </td>
-              <td>
-                <XButton />
-              </td>
-            </tr>
+          {this.state.rows.map(({ key, dish }, i) => (
+            <Row
+              key={key}
+              dish={dish}
+              onChange={newDish => this.replaceRow(i, newDish)}
+            />
           ))}
         </tbody>
         <tfoot>
           <tr>
-            <td><PlusButton /></td>
+            <td>
+              <PlusButton />
+            </td>
           </tr>
         </tfoot>
       </table>
