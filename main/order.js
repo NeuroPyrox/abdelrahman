c6182d512da4fec290dc3c938cfa416138a345dc
Glitch (abdelrahman-dev) <none>
2020-02-11 21:36:51 +0000

    Add add and changeSpiceLevel methods to order class

--- main/order.js
+++ main/order.js
@@ -1,16 +1,55 @@
 "use strict";
 
+// TODO factor out spiceQuantities into a separate class
+
 class Order {
   constructor() {
-    this.lines = []
+    this.dishes = [];
   }
-  
+
   getLines() {
-    return this.lines
+    const lines = [];
+    for (const {dishName, spiceQuantities} of this.dishes) {
+      for (const spiceLevel of ["notSpicy", "mild", "hot"]) {
+        const quantity = spiceQuantities[spiceLevel];
+        if (quantity) {
+          lines.push({dishName, spiceLevel, quantity})
+        }
+      }
+    }
+    return lines
   }
-  
-  add(_) {
-    this.lines.push(1);
+
+  add(dishName) {
+    const index = this.dishes.findIndex(dish => dish.dishName === dishName);
+    if (index === -1) {
+      this.dishes.push({dishName, spiceQuantities: {notSpicy: 1}});
+      return;
+    }
+    const spiceQuantities = this.dishes[index].spiceQuantities;
+    if (!spiceQuantities.notSpicy) {
+      spiceQuantities.notSpicy = 1;
+      return;
+    }
+    spiceQuantities.notSpicy += 1
+  }
+
+  changeSpiceLevel(dishName, oldLevel, newLevel) {
+    const index = this.dishes.findIndex(dish => dish.dishName === dishName);
+    if (index === -1) {
+      throw Error();
+    }
+    const spiceQuantities = this.dishes[index].spiceQuantities;
+    const oldQuantity = spiceQuantities[oldLevel];
+    if (!oldQuantity) {
+      throw Error();
+    }
+    spiceQuantities[oldLevel] = 0;
+    if(!spiceQuantities[newLevel]) {
+      spiceQuantities[newLevel] = oldQuantity;
+      return;
+    }
+    spiceQuantities[newLevel] += oldQuantity;
   }
 }
 
--- main/orderTest.js
+++ main/orderTest.js
@@ -1,20 +1,75 @@
 "use strict";
 
+// TODO make orders immutable
+// TODO make the spice level optional
+
 const Order = require("./order.js");
 const { assert, deepEqual } = require("../helpers.js");
 
+const assertLines = (order, lines) =>
+  assert(deepEqual(order.getLines(), lines));
+
 const order = new Order();
-// TODO replace with deepEqual
-assert(deepEqual(order.getLines(), []));
+assertLines(order, []);
 
 order.add("Sweet 'n Sour Chicken");
-assert(
-  // TODO test deepEqual
-  deepEqual(order.getLines(), [
-    { dishName: "Sweet 'n Sour Chicken", quantity: 1, spiceLevel: "notSpicy" }
-  ])
-);
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 1 }
+]);
 
-// TODO test multiple additions, changing the spice level, and deleting a line
+order.add("Sweet 'n Sour Chicken");
+order.add("Sweet 'n Sour Chicken");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 3 }
+]);
+
+order.add("Butter Chicken");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 3 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 1 }
+]);
+
+order.add("Butter Chicken");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 3 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
+]);
+
+order.changeSpiceLevel("Sweet 'n Sour Chicken", "notSpicy", "mild");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
+]);
+
+order.add("Sweet 'n Sour Chicken");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 1 },
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
+]);
+
+order.changeSpiceLevel("Sweet 'n Sour Chicken", "notSpicy", "hot");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "hot", quantity: 1 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
+]);
+
+order.add("Sweet 'n Sour Chicken");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 1 },
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "hot", quantity: 1 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
+]);
+
+order.changeSpiceLevel("Sweet 'n Sour Chicken", "notSpicy", "hot");
+assertLines(order, [
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "hot", quantity: 2 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
+]);
+
+// TODO test deleting a line
 
 module.exports = Promise.resolve();
