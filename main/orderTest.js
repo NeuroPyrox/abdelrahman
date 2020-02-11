508f887bcc02878022d3d8c85936ec93afb5066a
Glitch (abdelrahman-dev) <none>
2020-02-11 23:09:38 +0000

    Remove redundant tests

--- main/orderTest.js
+++ main/orderTest.js
@@ -21,58 +21,20 @@ assertLines(order, [
 ]);
 
 order.add("Sweet 'n Sour Chicken");
-order.add("Sweet 'n Sour Chicken");
 assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 3 }
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 2 }
 ]);
 
 order.add("Butter Chicken");
 assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 3 },
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 2 },
   { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 1 }
-]);
-
-order.add("Butter Chicken");
-assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 3 },
-  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
 ]);
 
 order.changeSpiceLevel("Sweet 'n Sour Chicken", "notSpicy", "mild");
 assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
-  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
-]);
-
-order.add("Sweet 'n Sour Chicken");
-assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 1 },
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
-  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
-]);
-
-order.changeSpiceLevel("Sweet 'n Sour Chicken", "notSpicy", "hot");
-assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "hot", quantity: 1 },
-  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
-]);
-
-order.add("Sweet 'n Sour Chicken");
-assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "notSpicy", quantity: 1 },
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "hot", quantity: 1 },
-  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
-]);
-
-order.changeSpiceLevel("Sweet 'n Sour Chicken", "notSpicy", "hot");
-assertLines(order, [
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 3 },
-  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "hot", quantity: 2 },
-  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 2 }
+  { dishName: "Sweet 'n Sour Chicken", spiceLevel: "mild", quantity: 2 },
+  { dishName: "Butter Chicken", spiceLevel: "notSpicy", quantity: 1 }
 ]);
 
 // TODO test deleting a line
-
-module.exports = Promise.resolve();
