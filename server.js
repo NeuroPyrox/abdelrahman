269e7d0a4b309e0d2eae6c3024b6648f9b9e85af
Glitch (abdelrahman-dev) <none>
2020-02-29 06:51:25 +0000

    Default to 404 error

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 const http = require("http");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Hello World!");
-}).listen(process.env.PORT);
+http
+  .createServer((req, res) => {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Page not found");
+  })
+  .listen(process.env.PORT);
 
 console.log(`Your app is listening on port ${process.env.PORT}`);
