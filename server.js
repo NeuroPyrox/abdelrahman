4676604baa24774e3a6841ff9a7d2c89360c6f03
Glitch (abdelrahman-dev) <none>
2020-02-29 02:56:01 +0000

    Prepare for major refactoring again

--- server.js
+++ server.js
@@ -1,80 +1,10 @@
 "use strict";
 
-require("./test.js");
+const http = require("http");
 
-const {asyncHandler} = require("./helpers.js")
-const database = require("./database.js")
-const startServer = require("./startServer.js");
-const services = require("./services.js");
-const bodyParser = require("body-parser");
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end("Hello World!");
+}).listen(process.env.PORT);
 
-const jsonParser = bodyParser.json()
-
-const menu = new database.Table("Menu", {
-  dishName: "TEXT",
-  imageUrl: "TEXT",
-  spicy: "BOOL"
-});
-const prices = new database.Table("Prices", { numMeals: "INT", price: "INT" });
-const adminPushSubscriptions = new database.Table("AdminPushSubscriptions", {
-  endpoint: "TEXT",
-  p256dh: "TEXT",
-  auth: "TEXT"
-});
-const orders = new database.Table("Orders", {
-  timestamp: "INT",
-  butterChickenQuantity: "INT",
-  butterChickenSpiceLevel: "TEXT",
-  sweetNSourQuantity: "INT",
-  delivery: "BOOL",
-  contactInfo: "TEXT"
-});
-
-// TODO this is the sloppiest authentication I can possibly think of. Revise it.
-const adminOnly = (req, res, next) => {
-  if (req.get("Authorization") !== "insecurePassword") {
-    res.sendStatus(403)
-  } else {
-    next()
-  }
-}
-
-const notifyAdmin = asyncHandler(async (req, res) => {
-  const rows = await adminPushSubscriptions();
-  const promises = rows
-        .map(push.unflattenSubscription)
-        .map(subscription => push.send(subscription, JSON.stringify(req.body)))
-  await Promise.all(promises)
-});
-
-// Use helper functions to interface with Expressjs
-// because otherwise, the middleware would make it unacceptably verbose
-startServer({
-  "/": services.staticFile("/main/index.html"),
-  "/admin/": services.redirect("/admin"),
-  "/admin": services.staticFile("/admin/index.html"),
-
-  "/main/bundle.js": services.staticFile("/main/bundle.js"),
-  "/main/style.css": services.staticFile("/main/style.css"),
-  "/admin/bundle.js": services.staticFile("/admin/bundle.js"),
-  "/admin/style.css": services.staticFile("/admin/style.css"),
-
-  "/public-vapid-key": services.staticJson({
-    key: process.env.PUBLIC_VAPID_KEY
-  }),
-
-  "/menu": services.table(menu).route({
-    get: ["main"],
-    put: [adminOnly, jsonParser, "main"]
-  }),
-  "/prices": services.table(prices).route({
-    get: ["main"],
-    put: [adminOnly, jsonParser, "main"]
-  }),
-  "/orders": services.table(orders).route({
-    post: [jsonParser, "main", notifyAdmin],
-    get: [adminOnly, "main"]
-  }),
-  "/admin-push-subscriptions/:endpoint": services.tableRow(adminPushSubscriptions)
-    .routeAll([adminOnly, jsonParser, "main"])
-});
+console.log(`Your app is listening on port ${process.env.PORT}`);
