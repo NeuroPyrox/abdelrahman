dfc189a77baa45b3161d1f0d782703a632b0e3c5
Glitch (abdelrahman-dev) <none>
2020-03-02 20:31:20 +0000

    Show main page title

--- server.js
+++ server.js
@@ -2,11 +2,28 @@
 
 const http = require("http");
 
+const mainPage = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Abdelrahman's Food</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+  </head>
+  <body>
+    Main page
+  </body>
+</html>
+`
+
 http
   .createServer((req, res) => {
     if (req.url == "/") {
       res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("Main");
+      res.end(mainPage);
     } else {
       res.writeHead(404, { "Content-Type": "text/html" });
       res.end("Page not found");
